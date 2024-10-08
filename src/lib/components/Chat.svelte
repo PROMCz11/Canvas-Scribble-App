8af140c1a88f57c0d83d8e62f474e6f0f577c029
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-10-08 17:57:05 +0300

    First instance of chat

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -1,20 +1,34 @@
 <script>
     let messages = [
-        {content: "galaxy"},
-        {content: "night"},
-        {content: "dog"},
-        {content: "laundry"},
-        {content: "chair"},
-        {content: "dispair"},
-        {content: "person"},
+        // {content: "galaxy"},
+        // {content: "night"},
+        // {content: "dog"},
+        // {content: "laundry"},
+        // {content: "chair"},
+        // {content: "dispair"},
+        // {content: "person"},
     ]
+
+    const sendMessage = content => {
+        // socket emit a message* event
+        messages = [...messages, {content: content}];
+    }
 </script>
 
 <div class="component">
-    <div class="message-container">
-
+    <div class="messages-container">
+        {#each messages as { content }}
+            <div class="message">
+                {content}
+            </div>
+        {/each}
     </div>
-    <input type="text" placeholder="Enter your guess">
+    <input on:keydown={e => {
+        if(e.key === "Enter") {
+            sendMessage(e.target.value);
+            e.target.value = "";
+        }
+    }} type="text" placeholder="Enter your guess">
 </div>
 
 <style>
@@ -24,5 +38,21 @@
         max-height: 100svh;
         overflow: auto;
         border-left: 1px solid white;
+
+        display: flex;
+        flex-direction: column;
+    }
+
+    input {
+        margin-top: auto;
+    }
+
+    .message {
+        margin-top: .5rem;
+        padding-bottom: .5rem;
+    }
+
+    .message:not(.message:last-of-type) {
+        border-bottom: 1px solid #757575;
     }
 </style>
