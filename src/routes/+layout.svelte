6d5d6106502a55f6f10b363b098ce3916b0f5bcf
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-10-21 11:26:47 +0300

    Added a progress bar loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import "../../src/global.css";
+    import { ProgressBar } from "@prgm/sveltekit-progress-bar";
 </script>
+
+<ProgressBar color="white" zIndex={100}/>
 
 <slot></slot>
